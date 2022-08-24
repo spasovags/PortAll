b69ca5f5218c4c6d0f8b6dea5c69bc33c4036723
Spasova <eexemplega@gmail.com>
2022-08-24 15:53:43 +0300

    hide nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,57 +1,62 @@
 import styles from '../styles/Navbar.module.css'
-import React from 'react'
-// import logo from '../components/icons/logo192.png'
+import { useState } from "react";
+import Image from 'next/image'
+import logo from './icons/logo.avif'
 
 const Navbar = () => {
-  function toggleNavMenu() {
-    const navMenu = document.querySelector('.nav-links-container')
-    navMenu.classList.toggle('hideMenu')
-    const menuIcon = document.querySelector('.menu-container')
-    menuIcon.classList.toggle('active')
-  }
+  // function toggleNavMenu() {
+    // const navMenu = document.querySelector('.nav-links-container')
+    // navMenu.classList.toggle('hideMenu')
+    // const menuIcon = document.querySelector('.menu-container')
+    // menuIcon.classList.toggle('active')
+
+    const [isModal, setIsModal] = useState(true);
+    const contentClassname = isModal
+        ? `${styles["hideMenu"]} ${styles["nav-links-container"]}`
+        : `${styles["nav-links-container"]}`;
+
   return (
     <div className={`${styles["nav-background"]}`}>
-      <div className={styles.navbar-container}>
-        <div className={styles.logo-close-container}>
+      <div className={`${styles["navbar-container"]}`}>
+        <div className={`${styles["logo-close-container"]}`}>
           <a href="#">
-            <img src={logo} width="auto" height="40px" alt="Logo" />
+            <Image src={logo} height={45} width={60} alt="Link home" />
           </a>
-          <div href="#" className={`${styles[menu-container]} 
-          ${styles[icon]}`} onClick={toggleNavMenu}>
-            <span className={styles.span-top}></span>
-            <span className={styles.span-middle}></span>
-            <span className={styles.span-bottom}></span>
+          <div href="#" className={`${styles["menu-container"]} 
+          ${styles["icon"]}`} onClick={() => setIsModal(!isModal)}>
+            <span className={`${styles["span-top"]}`}></span>
+            <span className={`${styles["span-middle"]}`}></span>
+            <span className={`${styles["span-bottom"]}`}></span>
           </div>
         </div>
-        <div className={`${styles[nav-links-container]} 
-        ${styles[hideMenu]}`}>
-          <div className="dropdown">
-            <button className="dropbtn">Dropdown</button>
-            <div className="dropdown-content">
+        <div className={contentClassname}>
+          <div className={`${styles["dropdown"]}`}>
+            <button className={`${styles["dropbtn"]}`}>Dropdown</button>
+            <div className={`${styles["dropdown-content"]}`}>
               <a href="#">Link 1</a>
               <a href="#">Link 2</a>
               <a href="#">Link 3</a>
             </div>
           </div>
-          <div className="dropdown">
-            <button className="dropbtn">Dropdown</button>
-            <div className="dropdown-content">
+          <div className={`${styles["dropdown"]}`}>
+            <button className={`${styles["dropbtn"]}`}>Dropdown</button>
+            <div className={`${styles["dropdown-content"]}`}>
               <a href="#">Link 1</a>
               <a href="#">Link 2</a>
               <a href="#">Link 3</a>
             </div>
           </div>
-          <div className="dropdown">
-            <button className="dropbtn">Dropdown</button>
-            <div className="dropdown-content">
+          <div className={`${styles["dropdown"]}`}>
+            <button className={`${styles["dropbtn"]}`}>Dropdown</button>
+            <div className={`${styles["dropdown-content"]}`}>
               <a href="#">Link 1</a>
               <a href="#">Link 2</a>
               <a href="#">Link 3</a>
             </div>
           </div>
-          <div className="dropdown">
-            <button className="dropbtn">Dropdown</button>
-            <div className="dropdown-content">
+          <div className={`${styles["dropdown"]}`}>
+            <button className={`${styles["dropbtn"]}`}>Dropdown</button>
+            <div className={`${styles["dropdown-content"]}`}>
               <a href="#">Link 1</a>
               <a href="#">Link 2</a>
               <a href="#">Link 3</a>
