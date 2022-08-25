1c9633a2ff3eb865b4877c3794ba506910d2e34f
Spasova <eexemplega@gmail.com>
2022-08-25 16:03:39 +0300

    home

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,30 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.parentHome}>
       <Head>
         <title>PortAll</title>
         <meta name="description" content="Find job, friends and sponsors" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-<h1>index.js</h1>
+      <div className={`${styles["card"]}`}>
+        <h1>Title - Card 1</h1>
+        <p>Medium length description. Let's add a few more words here.</p>
+        <div className={`${styles["photoCard"]}`}></div>
+      </div>
+      <div className={`${styles["childHome"]}`}>
+      <h1>Title - Card 1</h1>
+        <p>Medium length description. Let's add a few more words here.</p>
+        <div className={`${styles["photoCard"]}`}></div>
+      
+      </div>
+      <div className={`${styles["childHome"]}`}>
+      <h1>Title - Card 1</h1>
+        <p>Medium length description. Let's add a few more words here.</p>
+        <div className={`${styles["photoCard"]}`}></div>
+      
+      </div>
     </div>
   )
 }
