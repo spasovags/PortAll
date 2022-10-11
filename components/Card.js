36a80c13181e7d12ce6a0c046ea41bf3c4b206db
Spasova <eexemplega@gmail.com>
2022-10-11 15:58:17 +0300

    link added

--- components/Card.js
+++ components/Card.js
@@ -4,27 +4,34 @@ import Link from 'next/link'
 import styles from '../styles/Card.module.css'
 
 function Card(props) {
-  const profile = props.value;
+  const post = props.value;
   const basePath = '/images/PhotosProfiles'
 
    return (
       <section className={`${styles["card"]}`}>
         <div className={`${styles["profPicAndName"]}`}>
           <Link href="#">
-            <Image src={`${basePath}/${profile.profPic}`}
+            <Image src={`${basePath}/${post.profPic}`}
             width="70" 
             height="70"
             objectFit='cover'
             />
           </Link>
-          <Link href='#'>{profile.username}</Link>
+          <Link href='#'>{post.username}</Link>
         </div>
         <div className={`${styles["description"]}`}>
-          <p>{profile.description}</p>
-          <button className={`${styles["dropbtn"]}`}>read more</button>
+          <p>{post.description}</p>
+          <Link 
+            href={{
+              pathname: '/posts/[data]',
+              query: { data: post },
+            }}          
+            className={`${styles["dropbtn"]}`}>
+            read more
+          </Link>
         </div>
         <div className={`${styles["photoCard"]}`}>
-          <Image src={`${basePath}/${profile.photo}`} layout="fill" />
+          <Image src={`${basePath}/${post.photo}`} layout="fill" />
         </div>
       </section>
   )
