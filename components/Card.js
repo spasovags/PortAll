54149ae14946d77a70270d4f7e59fe4aac26b6cf
Spasova <eexemplega@gmail.com>
2022-10-28 10:47:50 +0300

    unexp

--- components/Card.js
+++ components/Card.js
@@ -37,7 +37,7 @@ const Card = (props) => {
           <p>{post.description}</p>
           <Link 
             href={{
-              pathname: '/posts/[data]',
+              pathname: '/posts/[...data]',
               query: { data: post.id },
             }}>
             <a
