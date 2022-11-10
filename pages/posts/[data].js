909820b9ed3c768d40d062de7f5051430f1c7a51
Spasova <eexemplega@gmail.com>
2022-11-10 15:09:11 +0200

    err link

--- pages/posts/[data].js
+++ pages/posts/[data].js
@@ -1,9 +1,62 @@
 import React from 'react'
-
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+import styles from '../../styles/Data.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+ 
+const basePath = '/images/PhotosProfiles'
+ 
+const fetcher = (url) => fetch(url).then((res) => res.json());
+ 
 const Post = () => {
+  const router = useRouter()
+ 
+  const { data, error } = useSWR('/api/staticdata', fetcher)
+  
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
+  const posts = JSON.parse(data)
+  let post = null
+  try {
+    post = posts.filter((post)=> post.id==router.query.data)
+    console.log('post : ', post)
+  } catch (err) {
+    console.log('Error: ', err.message);
+  }
+  if (error) return (<p>An error has occurred.</p>)
+  if (!post) return (<p>Loading...</p>)
+ 
   return (
-    <div>data</div>
+    <div className={`${styles["card"]}`}>
+    <div className={`${styles["profPicAndName"]}`}>
+    <Link href="#">
+      <a>
+      <Image src={`${basePath}/${post[0].profPic}`}
+      width="70"
+      height="70"
+      objectFit='cover'
+      />
+      </a>
+    </Link>
+    <Link href='#'><a>{post[0].username}</a></Link>
+    </div>
+    <div className={`${styles["description"]}`}>
+      <Link href="#" >
+       <a
+        className={`${styles["message-btn"]}`}>
+          send message &#9993;
+       </a>
+      </Link>
+      <p>{post[0].description}</p>
+    </div>
+    <div className={`${styles["photoCard"]}`}>
+          <Image src={`${basePath}/${post[0].photo}`} layout="fill" />
+    </div>
+  </div>
   )
 }
-
+ 
 export default Post
+
